2f1ce794c78025c308b3ccfe60bba090657a1f5a
Jennie-Myhren <jenniewalters2012@u.northwestern.edu>
2019-08-20 07:25:50 -0500

    added Encouragement fn

--- PracticePractice.js
+++ PracticePractice.js
@@ -1,3 +1,11 @@
+const Encouragement = () => {
+  console.log("If at first you don't succeed, try, try again!")
+}
+
+Encouragement();
+
 const PracticePractice = () => {
   console.log("PERFECT!");
 }
+
+PracticePractice();
